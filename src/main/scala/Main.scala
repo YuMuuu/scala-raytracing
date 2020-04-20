62a6441130068615dc610c4ac5943e9387c457fb
三浦勇 <c01150788f@edu.teu.ac.jp>
2020-04-21 01:03:06 +0900

    refactor

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -37,63 +37,49 @@ class HelloWorld {
   private def init(): Option[Long] = {
     GLFWErrorCallback.createPrint(System.err).set
     Option(glfwInit()) match {
-      case None => Left(throw new IllegalStateException("Unable to initialize GLFW"))
+      case None => {
+        throw new IllegalStateException("Unable to initialize GLFW")
+        System.exit(-1)
+      }
       case _ => {
-        // Configure GLFW
-        glfwDefaultWindowHints() // optional, the current window hints are already the default
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE) // the window will stay hidden after creation
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE) // the window will be resizable
+        glfwDefaultWindowHints()
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE)
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE)
       }
     }
 
     for {
       window: Long <- Option(glfwCreateWindow(300, 300, "Hello World!", NULL, NULL))
     } yield {
-      // Setup a key callback. It will be called every time a key is pressed, repeated or released.
       glfwSetKeyCallback(window, (window: Long, key: Int, scancode: Int, action: Int, mods: Int) => {
         if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) glfwSetWindowShouldClose(window, true) // We will detect this in the rendering loop
       })
 
-      // Get the thread stack and push a new frame
       Try {
-        val pWidth = stackPush.mallocInt(1) // int*
+        val pWidth = stackPush.mallocInt(1)
         val pHeight = stackPush.mallocInt(1)
-        // Get the window size passed to glfwCreateWindow
         glfwGetWindowSize(window, pWidth, pHeight)
-        // Get the resolution of the primary monitor
         val vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor)
-        // Center the window
         glfwSetWindowPos(window, (vidmode.width - pWidth.get(0)) / 2, (vidmode.height - pHeight.get(0)) / 2)
 
-        // the stack frame is popped automatically
         stackPush.close()
       }
 
-      // Make the OpenGL context current
       glfwMakeContextCurrent(window)
-      // Enable v-sync
       glfwSwapInterval(1)
-      // Make the window visible
       glfwShowWindow(window)
 
       window
     }
   }
 
-  private def loop(window: Long): Unit = { // This line is critical for LWJGL's interoperation with GLFW's
-    // OpenGL context, or any context that is managed externally.
-    // LWJGL detects the context that is current in the current thread,
-    // creates the GLCapabilities instance and makes the OpenGL
-    // bindings available for use.
+  private def loop(window: Long): Unit = {
     GL.createCapabilities
-    // Set the clear color
     glClearColor(1.0f, 0.0f, 0.0f, 0.0f)
-    // Run the rendering loop until the user has attempted to close
-    // the window or has pressed the ESCAPE key.
 
     while (!glfwWindowShouldClose(window)) {
-      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) // clear the framebuffer
-      glfwSwapBuffers(window) // swap the color buffers
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+      glfwSwapBuffers(window)
       glfwPollEvents()
     }
   }
